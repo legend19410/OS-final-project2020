69df90c7f551ec6c26b43d1425e536164dde9ae1
Jason Hunter <jahunter767@gmail.com>
2020-11-28 20:42:49 -0500

    Reworked it so that it's possible to close while a process is moving. Processes have issues requeuing

--- libs/RR.py
+++ libs/RR.py
@@ -12,17 +12,148 @@ class RR(Scheduler):
         super().__init__(window)
         self.TIME_QUANTUM = 15
         self.timeBeforeInterrupt = self.TIME_QUANTUM
-        self.newProcesses = []
-        self.finishedProcesses = []
+        self.state = "waiting"
+        self.STATE_DICT = {"waiting": self.waiting, \
+            "enqueue": self.enqueue, \
+            "requeue": self.requeue, \
+            "dequeue": self.dequeue, \
+            "execute": self.execute}
 
     def run(self):
-        self.updateWindow()
-        i = 0
-        process = self.processes[i]
+        """ Simulates scheduler using the processes given """
 
+        prevState = ""
         # Run until all processes have been executed
-        while (len(self.finishedProcesses) != len(self.processes)):
-            pygame.time.Clock().tick(5)  # frame rate 5 frames per second
+        while (self.finishedProcesses < len(self.processes)):
+            if (self.state != prevState):
+                print("State: " + self.state)
+                print("Time: " + str(self.time))
+                prevState = self.state
+
+            self.STATE_DICT[self.state]()
+            self.updateWindow()
+            time.sleep(0.1)
+
+    def spawnProcess(self):
+        """ Spawns a newly arrived process """
+
+        try:
+            new = self.processes[self.time]
+            self.processList.extend(new)
+            self.state = "enqueue"
+            return 1
+        except KeyError:
+            return 0
+
+    def waiting(self):
+        """ Checks for newly arrived processes and increments the clock """
+        self.spawnProcess()
+        self.time += 1
+
+    def enqueue(self):
+        """ Adds a newly spaawned process to the queue """
+
+        p = self.processList[self.nextProcess]
+        back = self.queue.getEndPtr()
+        if (p.frontX() < back):
+            dist = back - p.frontX()
+            if (dist > p.stepSize):
+                p.moveRight()
+            else:
+                p.moveRight(dist)
+                self.queue.enqueue(p)
+                self.nextProcess += 1
+                if (self.nextProcess == len(self.processList)):
+                    self.state = "dequeue"
+
+    def dequeue(self):
+        """ Removes a process from the queue and adds it to the CPU for execution """
+
+        # If the CPU isn't busy send a process to be executed
+        if (self.CPU.lock):
+            self.state = "execute"
+        else:
+            # Checks if cpu has been updated with the next process
+            p = self.CPU.getProcess()
+            if (p == None):
+                p = self.queue.dequeue(self.window)
+                self.CPU.setProcess(p)
+
+            dist = self.CPU.centerX() - p.centerX()
+            if (dist > p.stepSize):
+                if (dist <= (self.CPU.width() + p.width())//2):
+                    p.inCPU = True
+                p.moveRight()
+            else:
+                x, y = self.CPU.center()
+                p.setCenter(x, y)
+                self.CPU.lock = True
+                self.state = "execute"
+
+    def execute(self):
+        """ Decrements the burst time of the current process and interrupts
+            execution if the time slice has been exhausted"""
+
+        if (not self.CPU.lock):                 # The process finished before the time slice
+            self.processList.remove(self.CPU.getProcess())
+            self.CPU.setProcess(None)
+            self.state = "dequeue"
+        elif (self.timeBeforeInterrupt == 0):   # The time slice finished before the process
+            self.state = "requeue"
+        else:                                   # The process still has time to execute
+            self.CPU.execute()
+            self.time += 1
+            self.timeBeforeInterrupt -= 1
+            self.spawnProcess()
+
+    def requeue(self):
+        """ Moves the current process back into the queue and updates the CPU """
+
+        p = self.CPU.getProcess()
+        down = self.CPU.bottomY() + 10
+        left = self.queue.backX() - 90
+        up = self.queue.topY()
+        right = self.queue.getEndPtr()
+
+        # Move the process down out of the CPU
+        if (p.topY() < down):
+            if (p.bottomY() >= self.CPU.bottomY()):
+                p.inCPU = False
+            p.moveDown()
+
+        # Move the process to the left after it has been removed from the queue
+        if (p.topY() >= down):
+            if (p.backX() > left):
+                p.moveLeft()
+
+        # Move process up inline with queue after moving it behind the queue
+        print("left: " + str(p.backX() <= left))
+        if (p.backX() <= left):
+            dist = p.topY() - up
+
+            print("p: " + str(p) + " " + str(self.CPU.getProcess()))
+            print("step check: " + str(dist > p.stepSize))
+            if (dist > p.stepSize):
+                p.moveUp()
+            else:
+                p.moveUp(dist)
+
+        # Enqueue the process
+        print("top: " + str(p.topY() == up))
+        if (p.topY() == up):
+            dist = right - p.frontX()
+            if (dist > p.stepSize):
+                p.moveRight()
+            else:
+                p.moveRight(dist)
+                self.queue.enqueue(p)
+                self.CPU.setProcess(None)
+                self.CPU.lock = False
+                if (self.spawnProcess() == 0):
+                    self.state = "dequeue"
+
+    """
+            print("Len: " + str(len(self.finishedProcesses)))
             print("time: " + str(self.time))
 
             # Moves process back into the queue if it's time slice is up but
@@ -50,10 +181,10 @@ class RR(Scheduler):
             if (process[1] == self.time):
                 print("Process ID: " + str(i))
                 p = Process(process[0], process[1], process[2])
-                self.newProcesses.append(p)
+                self.processList.append(p)
                 p.draw(self.window, self.updateWindow)
 
-                """
+                " " "
                 start = p.frontX()
                 stop = self.queue.getEndPtr()
                 lastStep = (stop - start)%30
@@ -61,11 +192,11 @@ class RR(Scheduler):
                     p.moveRight(30)
                     p.draw(self.window, self.updateWindow)
                 p.moveRight(lastStep)
-                """
+                " " "
 
                 x = self.queue.getEndPtr()
                 y = p.topY()
-                p.moveTo(self.window, (x, y), 90, self.updateWindow)
+                p.moveTo(self.window, (x, y), 30, self.updateWindow)
 
                 self.queue.enqueue(p)
                 i += 1
@@ -94,6 +225,7 @@ class RR(Scheduler):
                             nextProcess.inCPU = True
                         nextProcess.moveRight(30)
                         nextProcess.draw(self.window, self.updateWindow)
+                        #time.sleep(0.25)
                     c = self.CPU.center()
                     nextProcess.setCenter(c[0], c[1])
                     self.CPU.setProcess(nextProcess)
@@ -103,8 +235,9 @@ class RR(Scheduler):
 
             # Increment the time if the processor is idle and no new
             # processes have been created
-            if ((len(self.newProcesses) == 0) and not self.CPU.lock):
+            if ((len(self.processList) == 0) and not self.CPU.lock):
                 self.time += 1
-            time.sleep(1)
+            time.sleep(0.25)
             #window.fill((255, 255, 255))
             pygame.display.update()
+    """
