69de5ddc66161d88438dbb53366b2ac321e81a0e
Jason Hunter <jahunter767@gmail.com>
2020-11-28 20:44:56 -0500

    Changed it so it accepts a list of processes in the constructor and changed the processes atribute to a dictionary indexed on arrival time

--- libs/Scheduler.py
+++ libs/Scheduler.py
@@ -1,21 +1,39 @@
+import pygame
+
 from libs.CPU import CPU
 from libs.Process import Process
 from libs.Queue import Queue
 from libs.Table import Table
 
 class Scheduler:
-    def __init__(self, window):
+    def __init__(self, window, processes=[(1,5,33), (2,2,32), \
+            (3,3,44), (4,9,27), (5,10,58), (6,20,34), (7,30, 80)]):
         #(process ID, arrival time, burst time)
-        self.processes = [(2,2,32), (3,3,44), (1,5,33), (4,9,27), \
-            (5,10,58), (6,20,34), (7,30, 80)]
+
+        # Dictionary of processes indexed on arrival time
+        self.processes = {}
+        for id, arrivalTime, burstTime in processes:
+            proc = Process(id, burstTime)
+            try:
+                self.processes[arrivalTime].append(proc)
+            except KeyError:
+                self.processes[arrivalTime] = [proc]
+
         self.CPU = CPU()
         self.queue = Queue()
-        self.table = Table(self.processes)
+        self.table = Table(processes)
         self.window = window
-        self.time = 1
+        self.time = 0
+
+        self.nextProcess = 0
+        self.processList = [] # Stores all active processes
+        self.finishedProcesses = 0
 
     def updateWindow(self):
         self.window.fill((255, 255, 255))
         self.CPU.draw(self.window)
         self.table.draw(self.window)
         self.queue.draw(self.window)
+        for p in self.processList:
+            p.draw(self.window)
+        pygame.display.update()
