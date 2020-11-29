f56248a2c676dd786c90e91c3e20180896ce2a19
Jason Hunter <jahunter767@gmail.com>
2020-11-29 03:31:31 -0500

    Implemented FCFS using the new design

--- libs/FCFS.py
+++ libs/FCFS.py
@@ -1,3 +1,4 @@
+import time
 import pygame
 
 from libs.CPU import CPU
@@ -7,61 +8,50 @@ from libs.Table import Table
 from libs.Scheduler import Scheduler
 
 class FCFS(Scheduler):
-    def __init__(self, window):
-        super().__init__(window)
-        self.lock = False
+    def __init__(self, window, mode="normal"):
+        super().__init__(window, mode)
+        self.STATE_DICT.update({"enqueue": self.enqueue, \
+            "execute": self.execute})
 
     def run(self):
-        self.updateWindow()
+        """ Simulates scheduler using the processes given """
 
-        self.createProcess()
-        self.drawAllProcesses()
-        self.moveProcesses()
-        self.executeProcess()
+        # Run until all processes have been executed
+        while (self.finishedProcesses < self.numProcesses):
+            print(self.state)
+            self.STATE_DICT[self.state]()
+            self.updateWindow()
+            self.closeGameOnQuit()
+            time.sleep(0.1)
 
-        pygame.display.update()
-        self.time += 0.25
+    def enqueue(self):
+        """ Adds a newly spaawned process to the queue """
 
-    def createProcess(self):
-        """ Check the arrival time of each process and create it if arrival
-            time match current time """
+        p = self.processList[self.nextProcess]
+        back = self.queue.getEndPtr()
+        if (p.frontX() < back):
+            dist = back - p.frontX()
+            if (dist > p.stepSize):
+                p.moveRight()
+            else:
+                p.moveRight(dist)
+                self.queue.enqueue(p)
+                self.nextProcess += 1
+                if (self.nextProcess == len(self.processList)):
+                    self.state = "dequeue"
 
-        for process in self.processes:
-            if process[1] == self.time:
-                p = Process(self.CPU, self.queue, process[0],process[2])
-                self.processList.append(p)
+    def execute(self):
+        """ Executes the current process to completion before removing
+            the process from the cpu and releasing the lock """
 
-    def drawAllProcesses(self):
-        """ Draw all processes on the screen """
-        for process in self.processList:
-            process.draw(self.window)
-
-    def executeProcess(self):
-        """ If lock is true and there is a current process assigned to the
-            cpu then decrease that process burst time until it becomes zero
-            before removingthe process from the cpu and releasing the lock """
-
-        if self.lock and self.CPU.getProcess():
-            self.CPU.getProcess().burstTime -= 1
-            if self.CPU.getProcess().burstTime == 0:
-                self.processList.remove(self.CPU.getProcess())
-                self.lock = False
-
-    def moveProcesses(self):
-        """ Iterate over all processes in the process list to determine if
-            they can move """
-
-        for i, proc in enumerate(self.processList):
-            if proc.frontX() > self.CPU.backX() + 60:  # this stops the process from moving pass the cpu
-                continue                            # once pass cpu start pos + the diameter of the process
-                                                    # then never move that process again.
-            if proc.frontX() == self.queue.frontX():  # if process at the front of the queue
-                if not self.lock:                   # if lock not on move process beyond front and set lock to true
-                    proc.advance()
-                    self.lock = True
-                else:
-                    pass                           # else dont move
-            else:                                  # else process not at front of the queue; worry about process infront
-                                                   # this checks if there is a process directly infront. If not move
-                if not(self.processList[i-1].backX()-self.processList[i-1].width()//2 == proc.backX()+proc.width()//2):
-                    proc.advance()
+        if (self.CPU.lock):
+            self.CPU.execute()
+            self.clock.increment()
+            self.spawnProcess()
+        else:
+            self.processList.remove(self.CPU.getProcess())
+            self.CPU.setProcess(None)
+            self.finishedProcesses += 1
+            print("\t\tFin count: " + str(self.finishedProcesses) + \
+                "\t" + str(self.clock.getTime()))
+            self.state = "dequeue"
